03345a2c7ea4407bb80dd04603c7a311bce84b7e
Saranya-hash-640 <saranyanm2012@gmail.com>
2025-11-24 11:08:43 +0530

    Number Pattern Generator.py

---
+++ Challenge 6.py
@@ -0,0 +1,16 @@
+
+
+def number_pattern_generator(num):
+    count = 1
+    for i in range(1,(num*2)):
+        print(count,end="\n")
+        if i < num:
+            count+=1
+        else:
+            count-=1
+        for j in range(1,count):
+            print(j,end="")
+
+number  = int(input("Enter the number:"))
+
+number_pattern_generator(number)
